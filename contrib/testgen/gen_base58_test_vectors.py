d3c4d2d80cf4ca1e64909471d3e7fc302b02eb7e
yezune <yezune@gmail.com>
2014-09-10 14:22:00 +0900

    update start letter

--- contrib/testgen/gen_base58_test_vectors.py
+++ contrib/testgen/gen_base58_test_vectors.py
@@ -15,12 +15,12 @@ import random
 from binascii import b2a_hex
 
 # key types
-PUBKEY_ADDRESS = 48
+PUBKEY_ADDRESS = 125 #48 # suwoncoin -> start with 's'
 SCRIPT_ADDRESS = 5
 PUBKEY_ADDRESS_TEST = 111
 SCRIPT_ADDRESS_TEST = 196
-PRIVKEY = 176
-PRIVKEY_TEST = 239
+PRIVKEY = PUBKEY_ADDRESS + 128
+PRIVKEY_TEST = PUBKEY_ADDRESS_TEST + 128
 
 metadata_keys = ['isPrivkey', 'isTestnet', 'addrType', 'isCompressed']
 # templates for valid sequences
